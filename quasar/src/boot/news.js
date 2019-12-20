fb672c224259cfd62e4c5eb4c5574c7b75683829
Celine Tran <celine@Celines-MBP.fritz.box>
2019-12-20 18:36:40 +0100

    build spa & automatic commit on git

--- quasar/src/boot/news.js
+++ quasar/src/boot/news.js
@@ -3,7 +3,7 @@ import { Notify } from 'quasar'
 Notify.setDefaults({
   position: 'bottom',
   timeout: 1000000,
-  color: 'grey-8',
+  color: 'red-10',
   icon: 'fas fa-holly-berry',
   multiLine: true,
   actions: [{ icon: 'close', color: 'white' }]
@@ -12,10 +12,10 @@ Notify.setDefaults({
 var untilDate = new Date(2020, 1, 2)
 if (new Date(untilDate.toDateString()) > new Date(new Date().toDateString())) {
   Notify.create(`Wir wünschen euch frohe Weihnachten und einen guten Rutsch ins neue Jahr! 
-  Wir haben auch an folgenden Tagen für euch geöffnet: 
+  Wir folgenden Tagen haben wir abweichende Öffnungszeiten: 
   24.12. GESCHLOSSEN | 
   25.12. 13 - 21 Uhr | 
   26.12. 13 - 21 Uhr | 
   31.12. 13 - 20.30 Uhr | 
-  01.01. 13 - 21 Uhr`)
+  01.01. 14 - 21 Uhr`)
 }
